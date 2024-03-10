f532353e288136f3a6bbeefe99ef70680d383ba3
Moose <moosecodes@gmail.com>
2024-03-09 16:24:28 -0800

    update hero text

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          moosecodes
+          moosecodes github page
         </a>
       </header>
     </div>
